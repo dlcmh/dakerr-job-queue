3559c403885004c56786cca9fbecaea4ad174777
David Chin <dlcmhd@me.com>
2021-09-19 18:40:29 +0800

    feature

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
-import { handleRequest } from './handler';
+import { Router } from 'itty-router'
+
+const router = Router()
+
+router.get('/', () => {
+  return new Response('Hello, world!')
+})
 
 addEventListener('fetch', (event: FetchEvent) => {
-  event.respondWith(handleRequest(event.request));
-});
+  event.respondWith(router.handle(event.request))
+})
