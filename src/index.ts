e81b4a023bf3d3808f8a858aec0955465746a766
David Chin <dlcmhd@me.com>
2021-09-19 18:42:19 +0800

    feature

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,10 @@ router.get('/', () => {
   return new Response('Hello, world!')
 })
 
+router.get('/exercise_buddy/crushed_it', () => {
+  return new Response('/exercise_buddy/crushed_it')
+})
+
 addEventListener('fetch', (event: FetchEvent) => {
   event.respondWith(router.handle(event.request))
 })
